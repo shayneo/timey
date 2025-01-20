4a725b5871667fa7de74b1800da172d83597df85
Shayne O'Sullivan <shayneo@pm.me>
2025-01-20 16:22:51 -0600

    Lint

--- src/time.tsx
+++ src/time.tsx
@@ -1,44 +1,42 @@
 import { ActionPanel, Action, List } from "@raycast/api";
 
 export default function Command() {
-  const now = new Date()
-  const ms = now.getTime()
-  const s = Math.round(ms / 1000)
+  const now = new Date();
+  const ms = now.getTime();
+  const s = Math.round(ms / 1000);
   const times = [
     {
-      label: 'ISO',
+      label: "ISO",
       value: now.toISOString(),
     },
     {
-      label: 'Unix (ms)',
+      label: "Unix (ms)",
       value: ms.toString(),
     },
     {
-      label: 'Unix (s)',
+      label: "Unix (s)",
       value: `${s}`,
     },
     {
-      label: 'Local',
+      label: "Local",
       value: now.toLocaleString(),
     },
-  ]
+  ];
   return (
     <List navigationTitle="Timey">
-      {times.map(t => {
-
-        return <List.Item
-          key={t.label}
-          title={t.label}
-          subtitle={t.value}
-          actions={
-            <ActionPanel>
-              <Action.CopyToClipboard
-                title="Copy"
-                content={t.value}
-              />
-            </ActionPanel>
-          }
-        />
+      {times.map((t) => {
+        return (
+          <List.Item
+            key={t.label}
+            title={t.label}
+            subtitle={t.value}
+            actions={
+              <ActionPanel>
+                <Action.CopyToClipboard title="Copy" content={t.value} />
+              </ActionPanel>
+            }
+          />
+        );
       })}
     </List>
   );
